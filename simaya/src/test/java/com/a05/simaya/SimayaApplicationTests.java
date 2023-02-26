eaaa74a9b0a262f8aa37e635ffc989fb843b04f2
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2023-02-26 17:07:39 +0700

    init spring

---
+++ simaya/src/test/java/com/a05/simaya/SimayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a05.simaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/a05/simaya/SimayaApplication.java
@@ -0,0 +1,13 @@
+package com.a05.simaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimayaApplication.class, args);
+	}
+
+}
