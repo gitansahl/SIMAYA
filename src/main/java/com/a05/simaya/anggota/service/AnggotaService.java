d23f4399aac94396cad56549404f9e44027d814a
Marvel <marvelphannn@gmail.com>
2023-03-08 22:20:51 +0700

    feat: tambah event

--- src/main/java/com/a05/simaya/anggota/service/AnggotaService.java
+++ src/main/java/com/a05/simaya/anggota/service/AnggotaService.java
@@ -1,7 +1,12 @@
 package com.a05.simaya.anggota.service;
 
+import com.a05.simaya.anggota.model.AnggotaModel;
 import com.a05.simaya.anggota.payload.CreateAnggotaDTO;
+
+import java.util.List;
 
 public interface AnggotaService {
     void tambahAnggota(CreateAnggotaDTO anggota);
+
+    List<AnggotaModel> getListAnggota();
 }
