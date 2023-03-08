09af9a0f91ef05e0b8ce4c849fb6b92b7f7e69ef
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2023-03-08 12:59:43 +0700

    feat: add model for event, progres, anggota

---
+++ src/main/java/com/a05/simaya/anggota/model/GolDarEnum.java
@@ -0,0 +1,5 @@
+package com.a05.simaya.anggota.model;
+
+public enum GolDarEnum {
+    A, AB, O, B
+}
---
+++ src/main/java/com/a05/simaya/anggota/model/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.a05.simaya.anggota.model;
+
+public enum RoleEnum {
+    ADMIN,
+    SDM_OPERASIONAL,
+    PENGEMBANGAN_USAHA,
+    ANGGOTA,
+}
