acb0a208d9d38fee04c9246659bbbea60a58554a
Gitan Sahl Tazakha Wijaya <gitan.sahl@ui.ac.id>
2023-03-15 00:17:21 +0700

    Branch gitan

--- src/main/java/com/a05/simaya/anggota/controller/AnggotaController.java
+++ src/main/java/com/a05/simaya/anggota/controller/AnggotaController.java
@@ -1,7 +1,11 @@
 package com.a05.simaya.anggota.controller;
 
+import com.a05.simaya.anggota.model.AnggotaModel;
+import com.a05.simaya.anggota.model.ProfileModel;
 import com.a05.simaya.anggota.payload.AnggotaDTO;
+import com.a05.simaya.anggota.repository.AnggotaDb;
 import com.a05.simaya.anggota.service.AnggotaService;
+import com.a05.simaya.event.model.DirektoratEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 public class AnggotaController {
@@ -67,5 +73,45 @@ public class AnggotaController {
     @GetMapping(value = "/anggota/viewall")
     public String viewAllAnggota() {
         return "anggota/daftar-anggota";
+    }
+
+    @GetMapping(value = "/profil")
+    public String profilPage(Model model,
+                             Principal principal) {
+        AnggotaModel anggota = anggotaService.getAnggotaByUsername(principal.getName());
+
+        model.addAttribute("anggota", anggota);
+        model.addAttribute("aset", getAset(anggota.getProfile()));
+        model.addAttribute("divisi", getDivisi(anggota.getProfile().getDivisi()));
+
+        return "anggota/profile";
+    }
+
+    public String getAset(ProfileModel profile) {
+        String res = "-";
+
+        if (profile.getIsPunyaMobil().equals(Boolean.TRUE)) {
+            res = "Mobil";
+        }
+
+        if (profile.getIsPunyaMotor().equals(Boolean.TRUE)) {
+            res = res.equals("-") ? res + ", Motor" : "Motor";
+        }
+
+        if (profile.getIsPunyaRumah().equals(Boolean.TRUE)) {
+            res = res.equals("-") ? res + ", Rumah" : "Rumah";
+        }
+
+        if (profile.getIsPunyaVila().equals(Boolean.TRUE)) {
+            res = res.equals("-") ? res + ", Vila" : "Vila";
+        }
+        return res;
+    }
+
+    public String getDivisi(DirektoratEnum direktorat) {
+        if (direktorat == null) return "Unknown";
+        if (direktorat.equals(DirektoratEnum.KEUANGAN_PROGRAM)) return "Keuangan dan Program";
+        if (direktorat.equals(DirektoratEnum.SDM_OPERASIONAL)) return "SDM dan Operasional";
+        else return "Pengembangan Usaha";
     }
 }
