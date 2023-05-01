2763c1156adaadffd3fcb13d3928dfe438cab732
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2023-05-01 19:29:47 +0700

    feat: add jenis ziswaf enum value and no telp on ziswaf model

--- src/main/java/com/a05/simaya/ziswaf/model/JenisZiswafEnum.java
+++ src/main/java/com/a05/simaya/ziswaf/model/JenisZiswafEnum.java
@@ -1,4 +1,11 @@
 package com.a05.simaya.ziswaf.model;
 
 public enum JenisZiswafEnum {
+    ZAKAT_PENGHASILAN,
+    ZAKAT_MAAL,
+    ZAKAT_FITRAH,
+    INFAQ,
+    SEDEKAH,
+    KAFALAH_YATIM,
+    WAKAF
 }
