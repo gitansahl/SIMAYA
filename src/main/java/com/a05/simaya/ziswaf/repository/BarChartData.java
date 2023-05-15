1e1522c468b8fbaf7c06d75db27b7799bfb0ddb1
Gitan Sahl Tazakha Wijaya <gitan.sahl@ui.ac.id>
2023-05-15 15:04:29 +0700

    feat: fitur statistik dashboard ziswaf

---
+++ src/main/java/com/a05/simaya/ziswaf/repository/BarChartData.java
@@ -0,0 +1,9 @@
+package com.a05.simaya.ziswaf.repository;
+
+import com.a05.simaya.ziswaf.model.JenisZiswafEnum;
+
+public interface BarChartData {
+    JenisZiswafEnum getJenis();
+
+    Long getJumlahData();
+}
