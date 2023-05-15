77a0e21f1fbd2993613482c75ce561c3410ebaab
Gitan Sahl Wijaya <gitan.sahl@ui.ac.id>
2023-05-15 15:03:19 +0700

    feat: fitur statistik dashboard ziswaf

--- src/main/java/com/a05/simaya/ziswaf/service/ZiswafService.java
+++ src/main/java/com/a05/simaya/ziswaf/service/ZiswafService.java
@@ -4,10 +4,15 @@ import com.a05.simaya.ziswaf.model.ZiswafModel;
 import com.a05.simaya.ziswaf.payload.ZiswafDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ZiswafService {
     ZiswafModel tambahDonasiZizwaf(ZiswafDTO zizwafDTO);
     Long getZiswafToday();
     Long getZiswafLastWeek();
     Long getZiswafLastMonth();
+    Object getBarChartData();
+
+    Object getLineChartDataDaily();
+    Object getLineChartDataMonthly();
 }
